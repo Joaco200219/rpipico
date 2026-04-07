53d30a698db0a8693ada13d46c523f52786abb1a
Joaco200219 <salamecondulcedeleche@gmail.com>
2026-04-07 16:58:14 -0300

    "Primeros cambios en main"

--- main.py
+++ main.py
@@ -13,12 +13,12 @@
 # red LED: ON == WiFi fail
 # green LED heartbeat: demonstrates scheduler is running.
 
-from mqtt_as import MQTTClient
+from mqtt_as import MQTTClient, config
 from mqtt_local import config
 import uasyncio as asyncio
 import dht, machine
 
-d = dht.DHT22(machine.Pin(13))
+d = dht.DHT22(machine.Pin(15))
 
 def sub_cb(topic, msg, retained):
     print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
@@ -29,8 +29,12 @@ async def wifi_han(state):
 
 # If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
 async def conn_han(client):
-    await client.subscribe('topico/temperatura', 1)
-    await client.subscribe('topico/humedad', 1)
+    await client.subscribe('riedmaier/temperatura', 1)
+    await client.subscribe('riedmaier/humedad', 1)
+
+config["queue_len"] = 1  # Use event interface with default queue size
+MQTTClient.DEBUG = True  # Optional: print diagnostic messages
+client = MQTTClient(config)
 
 async def main(client):
     await client.connect()
@@ -41,12 +45,12 @@ async def main(client):
             d.measure()
             try:
                 temperatura=d.temperature()
-                await client.publish('topico/temperatura', '{}'.format(temperatura), qos = 1)
+                await client.publish('riedmaier/temperatura', '{}'.format(temperatura), qos = 1)
             except OSError as e:
                 print("sin sensor temperatura")
             try:
                 humedad=d.humidity()
-                await client.publish('topico/humedad', '{}'.format(humedad), qos = 1)
+                await client.publish('riedmaier/humedad', '{}'.format(humedad), qos = 1)
             except OSError as e:
                 print("sin sensor humedad")
         except OSError as e:
@@ -58,10 +62,13 @@ config['subs_cb'] = sub_cb
 config['connect_coro'] = conn_han
 config['wifi_coro'] = wifi_han
 config['ssl'] = True
+config['queue_len'] = 1  # Use event interface with default queue size
 
 # Set up client
-MQTTClient.DEBUG = True  # Optional
-client = MQTTClient(config)
+MQTTClient.DEBUG = True  
+client = MQTTClient(config) # Funcionan estas configuraciones?
+
+
 try:
     asyncio.run(main(client))
 finally:
