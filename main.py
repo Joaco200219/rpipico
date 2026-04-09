c004562a59544f3734ee3dbadcdf182ade172d62
Joaco200219 <salamecondulcedeleche@gmail.com>
2026-04-09 17:01:59 -0300

    publica destello y periodo

--- main.py
+++ main.py
@@ -1,74 +1,118 @@
 from mqtt_as import MQTTClient, config
 from mqtt_local import config
 import uasyncio as asyncio
+#import network
+#import ubinascii  #estas dos lineas use para la mac
 import dht, machine
-import btree
+import json
 
+
+led_interno = machine.Pin("LED", machine.Pin.OUT)
 d = dht.DHT22(machine.Pin(15))
-# BDD
+
+DB_FILE = "midb.json"
+db_cache = {}
+
+# Cargar la base de datos al iniciar
 try:
-    f = open("mydb", "r+b")
+    with open(DB_FILE, "r") as f:
+        db_cache = json.load(f)
 except OSError:
-    f = open("mydb", "w+b")
+    db_cache = {}  # Si no existe, inicia vacía
 
-# Now open a database itself
-db = btree.open(f)
+def get_db(key, default):
+    if key in db_cache:
+        return db_cache[key]
+    else:
+        set_db(key, default)
+        return default
 
-# db[b"temperatura"]
-# db[b"humedad"]
-# db[b"setpoint"]
-# db[b"periodo"]
-# db[b"modo"]
+def set_db(key, val):
+    db_cache[key] = val
+    # Guardar en la memoria flash
+    with open(DB_FILE, "w") as f:
+        json.dump(db_cache, f)
 
-def sub_cb(topic, msg, retained):
-    print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
+async def parpadear():
+    for _ in range(5):  # Destella 5 veces (2.5 segundos en total)
+        led_interno.on()
+        await asyncio.sleep(0.25)
+        led_interno.off()
+        await asyncio.sleep(0.25)
 
-async def wifi_han(state):
-    print('Wifi is ', 'up' if state else 'down')
-    await asyncio.sleep(1)
+async def messages(client):  # Quitamos 'datos'
+    async for topic, msg, retained in client.queue:
+        comando = topic.decode().split('/')[-1]
+        val = msg.decode()
+        
+        # 1. Guardar SOLO los parámetros permitidos en la BD no volátil
+        if comando in ["setpoint", "periodo", "modo", "rele"]:
+            set_db(comando, val)
+            print(f"Guardado en BD {comando}: {val}")
 
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
-async def conn_han(client):
-    await client.subscribe('riedmaier/temperatura', 1)
-    await client.subscribe('riedmaier/humedad', 1)
+        # 2. Ejecutar acciones que NO se guardan en la BD
+        if comando == "destello":
+            asyncio.create_task(parpadear())
+            print("Acción: Destellando...")
+        
 
-config["queue_len"] = 1  # Use event interface with default queue size
-MQTTClient.DEBUG = True  # Optional: print diagnostic messages
-client = MQTTClient(config)
+async def up(client):
+    while True:
+        await client.up.wait()
+        client.up.clear()
+        await client.subscribe('28:cd:c1:04:d8:a7/setpoint', 1)
+        await client.subscribe('28:cd:c1:04:d8:a7/periodo', 1)
+        await client.subscribe('28:cd:c1:04:d8:a7/destello', 1)
+        await client.subscribe('28:cd:c1:04:d8:a7/modo', 1)
+        await client.subscribe('28:cd:c1:04:d8:a7/rele', 1)
 
 async def main(client):
     await client.connect()
+    asyncio.create_task(up(client))
+    asyncio.create_task(messages(client))
     n = 0
     await asyncio.sleep(2)  # Give broker time
     while True:
+        #mac = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode() #Obtiene mac
+        #print(mac) #muestra mac
+        temperatura = None
+        humedad = None
         try:
             d.measure()
             try:
                 temperatura=d.temperature()
-                await client.publish('riedmaier/temperatura', '{}'.format(temperatura), qos = 1)
             except OSError as e:
                 print("sin sensor temperatura")
             try:
                 humedad=d.humidity()
-                await client.publish('riedmaier/humedad', '{}'.format(humedad), qos = 1)
             except OSError as e:
                 print("sin sensor humedad")
+
+            datos = {
+                "temp": d.temperature(),
+                "hum": d.humidity(),
+                "setpoint": float(get_db("setpoint", 24.0)),
+                "modo": get_db("modo", "auto"),
+                "rele": int(get_db("rele", 1)),
+                "periodo": int(get_db("periodo", 20))
+            }
+
+            json_datos = json.dumps(datos)
+            id_dispositivo = "28:cd:c1:04:d8:a7"
+            await client.publish(f"{id_dispositivo}/", json_datos, qos=1)    
+            print("Publicado:", json_datos)
+
         except OSError as e:
             print("sin sensor")
-        await asyncio.sleep(20)  # Broker is slow
+        await asyncio.sleep(datos["periodo"])  # Broker is slow
 
 # Define configuration
-config['subs_cb'] = sub_cb
-config['connect_coro'] = conn_han
-config['wifi_coro'] = wifi_han
 config['ssl'] = True
 config['queue_len'] = 1  # Use event interface with default queue size
 
 # Set up client
 MQTTClient.DEBUG = True  
-client = MQTTClient(config) # Funcionan estas configuraciones?
-
-
+client = MQTTClient(config) # Funcionan estas configuraciones
 try:
     asyncio.run(main(client))
 finally:
